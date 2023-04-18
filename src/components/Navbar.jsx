13ad09428fce6ae56457b53579c614ea240cc3b4
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2023-04-18 12:26:07 +0100

    animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
       {/* mobile navbar */}
       <div className='flex items-center justify-end flex-1 sm:hidden'>
         <img src={toggle ? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain' onClick={() => setToggle((prev) => !prev)} />
-        <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded sidebar`}>
+        <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 left-0 mx-auto justify-center my-2 w-[90%] rounded sidebar`}>
         <ul className='flex flex-col items-center justify-center list-none' >
           {navLinks.map((nav, index)=> 
             <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-white hover:text-secondary ${index === navLinks.length - 1 ? "mr-0": 'mb-4'}`}>
