e3a4c71ca007a1a84fe3e52292cfa47afa13c29a
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2023-04-23 09:23:41 +0100

    document formatting update

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const Button = ({styles}) => {
+const Button = ({ styles }) => {
   return (
     <div>
-      <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-lg ${styles} hover:transform hover:scale-125 hover:skew-y-1`}>
+      <button
+        type="button"
+        className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-lg ${styles} hover:transform hover:scale-125 hover:skew-y-1`}
+      >
         Get Started
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,57 @@
-import {useState} from 'react'
-import {close, logo, menu} from "../assets"
-import {navLinks} from "../constants"
-
+import { useState } from "react";
+import { close, logo, menu } from "../assets";
+import { navLinks } from "../constants";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
-  
+
   return (
-    <nav className='flex items-center justify-between w-full py-6 navbar'>
-      <img src={logo} alt="hoobank" className='w-[124px] h-[32px]'/>
+    <nav className="flex items-center justify-between w-full py-6 navbar">
+      <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
       {/* desktop navbar */}
-      <ul className='items-center justify-end flex-1 hidden list-none sm:flex' >
-        {navLinks.map((nav, index)=> 
-          <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-white hover:text-secondary ${index === navLinks.length - 1 ? "mr-0": "mr-10"}`}>
-            <a href={`#${nav.id}`}>
-              {nav.title}
-            </a>
+      <ul className="items-center justify-end flex-1 hidden list-none sm:flex">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] text-white hover:text-secondary ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            }`}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
           </li>
-         )}
+        ))}
       </ul>
-      
+
       {/* mobile navbar */}
-      <div className='flex items-center justify-end flex-1 sm:hidden'>
-        <img src={toggle ? close : menu} alt="menu" className='w-[28px] h-[28px] object-contain cursor-pointer' onClick={() => setToggle((prev) => !prev)} />
-        <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 left-0 mx-auto justify-center my-2 w-[90%] rounded sidebar`}>
-        <ul className='flex flex-col items-center justify-center list-none' >
-          {navLinks.map((nav, index)=> 
-            <li key={nav.id} className={`font-poppins font-normal cursor-pointer text-[16px] text-white hover:text-secondary ${index === navLinks.length - 1 ? "mr-0": 'mb-4'}`} onClick={() => setToggle((prev) => !prev)}>
-              <a href={`#${nav.id}`}>
-                {nav.title}
-              </a>
-            </li>
-          )}
-        </ul>
+      <div className="flex items-center justify-end flex-1 sm:hidden">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain cursor-pointer"
+          onClick={() => setToggle((prev) => !prev)}
+        />
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute top-20 right-0 left-0 mx-auto justify-center my-2 w-[90%] rounded sidebar`}
+        >
+          <ul className="flex flex-col items-center justify-center list-none">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] text-white hover:text-secondary ${
+                  index === navLinks.length - 1 ? "mr-0" : "mb-4"
+                }`}
+                onClick={() => setToggle(false)}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar 
+export default Navbar;
